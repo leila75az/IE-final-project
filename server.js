71b38a5f5b55dc3d787cfb7a1b47b676aebfcfb9
leila75az <l.azimi75@yahoo.com>
2019-01-20 20:02:50 +0100

    add auth

--- server.js
+++ server.js
@@ -1,35 +1,94 @@
 'use strict';
-
+const Bcrypt = require('bcrypt');
 const Hapi=require('hapi');
-
+const AuthBearer = require('hapi-auth-bearer-token');
 // Create a server with a host and port
 const server=Hapi.server({
   host:'localhost',
   port:8000
 });
 
-// Add the route
-server.route({
-  method:'GET',
-  path:'/hello',
-  handler:function(request,h) {
-
-    return'hello world';
+const users = {
+  john: {
+    username: 'john',
+    password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+    name: 'John Doe',
+    id: '2133d32a',
+    token:'sdfsdfsdfkjskjdfkjskdfjsdfsdfsdf'
   }
-});
-
-// Start the server
-const start =  async function() {
-
-  try {
-    await server.start();
-  }
-  catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  console.log('Server running at:', server.info.uri);
 };
+
+const validate = async (request, username, password) => {
+
+  const user = users[username];
+  if (!user) {
+    return { credentials: null, isValid: false };
+  }
+
+  const isValid = await Bcrypt.compare(password, user.password);
+  const credentials = { id: user.id, name: user.name , token: user.token};
+
+  return { isValid, credentials };
+};
+
+
+const start = async () => {
+
+  const server = Hapi.server({ port: 8000 });
+
+  await server.register(require('hapi-auth-basic'));
+
+  server.auth.strategy('simple', 'basic', { validate });
+
+
+  await server.register(AuthBearer)
+
+  server.auth.strategy('token', 'bearer-access-token', {
+    allowQueryToken: true,              // optional, false by default
+    validate: async (request, token, h) => {
+
+      // here is where you validate your token
+      // comparing with token from your database for example
+      const isValid = token === '1234';
+
+      const credentials = { token };
+      const artifacts = { test: 'info' };
+
+      return { isValid, credentials, artifacts };
+    }
+  });
+
+
+  server.route({
+    method: 'GET',
+    path: '/login',
+    options: {
+      auth: 'simple'
+    },
+    handler:(request, h)=>{
+      console.log("h",h.request.auth)
+      return {"status":"200","token":h.request.auth.credentials.token}
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    options: {
+      auth: 'token'
+    },
+    handler:(request, h)=>{
+      console.log("h",h.request.auth)
+      return {"status":"200","token":h.request.auth.credentials.token}
+    }
+  });
+
+
+
+  await server.start();
+
+  console.log('server running at: ' + server.info.uri);
+};
+
 
 start();
